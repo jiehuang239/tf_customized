f383f8e018749e51be12fcd78e33edc949bcc000
jiehuang239 <46855687+jiehuang239@users.noreply.github.com>
2019-11-10 13:05:26 -0500

    Update rgb2greyscale.cc

--- rgb2greyscale.cc
+++ rgb2greyscale.cc
@@ -20,27 +20,24 @@ template<typename Device,typename T>
 class rgb_greyscale : public OpKernel {
     public:
       explicit rgb_greyscale(OpKernelConstruction* context):OpKernel(context) {}
-      void compute(OpKernelContext* context)override {
+      void compute( OpKernelContext* context) override {
           //your can access input and output tensors from context
           //grab the input tensor
           const Tensor& input_tensor = context->input(0);
+	  auto input = input_tensor.flat<float>();
           //create an output tensor
           Tensor* output_tensor = NULL; 
           OP_REQUIRES_OK(context,context->allocate_output(0,input_tensor.shape(),&output_tensor));
+	  auto output = output_tensor->template flat<float>(); 
           //get in/out pointers and their size
           //const T* input_ptr = input_tensor.flat<T>().data();
           //T*output_ptr = output_tensor->flat<T>().data();
           //int num_elements = input_tensor.NumElements();
           //Do the computation
-	  int i=0;
-	  i=i+1;
-          OP_REQUIRES(context, input_tensor.NumElements() <= tensorflow::kint32max,
-                errors::InvalidArgument("Too many elements in tensor"));
-          rgb_greyscaleFunctor<Device, T>()(
-                context->eigen_device<Device>(),
-                static_cast<int>(input_tensor.NumElements()),
-                input_tensor.flat<T>().data(),
-                output_tensor->flat<T>().data());          
+	  const int n = input.size()/3;
+	  for (int i = 1;i< N;i++) {
+	  	output(i) = input(3*i)*0.21+input(3*i+1)*0.71+input(3*i+2);
+	  }
       } 
     
 };
@@ -50,7 +47,7 @@ class rgb_greyscale : public OpKernel {
       Name("rgb2greyscale").Device(DEVICE_CPU).TypeConstraint<T>("T"), \
       rgb_greyscale<CPUDevice, T>);
 REGISTER_CPU(float);
-//REGISTER_CPU(int32);
+REGISTER_CPU(int32);
 
 // Register the GPU kernels.
 #ifdef GOOGLE_CUDA
@@ -61,5 +58,5 @@ REGISTER_CPU(float);
       Name("rgb2greyscale").Device(DEVICE_GPU).TypeConstraint<T>("T"), \
       rgb_greyscale<GPUDevice, T>);
 REGISTER_GPU(float);
-//REGISTER_GPU(int32);
+REGISTER_GPU(int32);
 #endif  // GOOGLE_CUDA
