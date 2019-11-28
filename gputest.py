6633f14887514c8ded6f21f6babe7bc06ac67da1
jiehuang239 <jh4000@columbia.edu>
2019-11-28 16:08:53 +0000

    saved

--- gputest.py
+++ gputest.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+tf.load_library('/usr/local/lib/python3.6/dist-packages/tensorflow/libtensorflow_framework.so.1')
 rgb2grey_module = tf.load_op_library('./rgb2greyscale.so')
 grey = rgb2grey_module.rgb2greyscale([21,234,45,234,12,67])
 with tf.Session() as sess:
